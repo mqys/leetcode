8c3ed29a41646e20dd41b9c01a63c906999511cd
mqys <1305111800@qq.com>
2016-02-08 21:00:08 +0800

    2016年 2月 8日 星期一 21时00分08秒 CST:

---
+++ bulbSwitcher.cc
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        // --- 0 ms---
+        // have only odd divisor
+        // divisor come in pairs, like 12 = 1*12 = 2*6 = 3*4
+        // count the perfect square
+        // sqrt(n) is the max num whose square is in the range
+        return sqrt(n);
+        
+    }
+};
